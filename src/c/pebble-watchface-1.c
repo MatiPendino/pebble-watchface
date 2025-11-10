8df48f877efac714359e3592a2f23bcc44ee0746
Mati <matiaspendino76@gmail.com>
2025-11-10 16:14:29 +0100

    feat: Micro5 font added

--- src/c/pebble-watchface-1.c
+++ src/c/pebble-watchface-1.c
@@ -2,6 +2,7 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static GFont s_time_font;
 
 static void update_time() {
   // Get a tm structure
@@ -22,6 +23,9 @@ static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+  // Create GFont
+  s_time_font = fonts_load_custom_font(resource_get_handle(MICRO5_24));
+
   // Create the TextLayer with specific bounds
   s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
 
@@ -32,12 +36,16 @@ static void main_window_load(Window *window) {
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
+  // Apply custom font to TextLayer
+  text_layer_set_font(s_time_layer, s_time_font);
+
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
+  fonts_unload_custom_font(s_time_font);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
