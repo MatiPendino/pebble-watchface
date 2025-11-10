377facee7ce7460f1c925fdded5bc61744cefc9b
Mati <matiaspendino76@gmail.com>
2025-11-10 16:34:25 +0100

    fix: Micro font name changed in code. Default text_layer_set_font removed

--- src/c/pebble-watchface-1.c
+++ src/c/pebble-watchface-1.c
@@ -24,7 +24,7 @@ static void main_window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 
   // Create GFont
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_MICRO5_24));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_MICRO_42));
 
   // Create the TextLayer with specific bounds
   s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
@@ -33,7 +33,7 @@ static void main_window_load(Window *window) {
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  //text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
   // Apply custom font to TextLayer
